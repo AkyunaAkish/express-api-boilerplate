e983d7ab47ccce5d18dacc6c5caa2cf1efac997b
Akyuna Akish <akyunaakish@gmail.com>
2018-06-06 11:16:54 -0600

    created example action

---
+++ server/components/endpoints/actions/placholderAction.js
@@ -0,0 +1,10 @@
+// actions can be used to create
+// re-usable functionality between 
+// your express route controllers
+
+module.exports = (someData) => {
+    // do something interesting
+    // with the data, if there is data
+    // return relevant data
+    return someData;
+};
